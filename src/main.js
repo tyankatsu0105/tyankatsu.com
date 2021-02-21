13f6751be0411da8b957b91060a7751d0086f6c6
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-02-21 15:33:04 +0900

    feat: add tag manager

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue";
+import * as Shared from "~/shared";
 
 import "@/styles/core/fonts.scss";
 import "@/styles/core/reset.scss";
 import "@/styles/core/base.scss";
 
 import store from "./store";
+
+if (process.browser) {
+  Shared.TagManager();
+}
 
 export default function (Vue, { router, head, isClient, appOptions }) {
   appOptions.store = store;
---
+++ src/shared/tagManager.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+export default function () {
+  if (process.env.NODE_ENV === "production") {
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src =
+        "https://www.googletagmanager.com/gtm.js?id=" +
+        i +
+        dl +
+        "&gtm_auth=fX__YPxnkT_M0Lvh8lj5EA&gtm_preview=env-5&gtm_cookies_win=x";
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-T3MJNJH");
+  } else if (process.env.NODE_ENV === "develop") {
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src =
+        "https://www.googletagmanager.com/gtm.js?id=" +
+        i +
+        dl +
+        "&gtm_auth=uaOJNKXxoyr5WGB7cQ6g7g&gtm_preview=env-6&gtm_cookies_win=x";
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-T3MJNJH");
+  }
+}
