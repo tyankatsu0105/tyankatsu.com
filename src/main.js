a2b3240bfb226eb147c65e226b60f6a566a69d01
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-02-27 17:34:24 +0900

    feat: use google fonts

--- src/main.js
+++ src/main.js
@@ -3,13 +3,26 @@
 
 import DefaultLayout from "~/layouts/Default.vue";
 
-import "@/styles/core/fonts.scss";
 import "@/styles/core/reset.scss";
 import "@/styles/core/base.scss";
 
 import store from "./store";
 
 export default function (Vue, { router, head, isClient, appOptions }) {
+  head.link.push({
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+  });
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=DotGothic16&display=swap",
+  });
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap",
+  });
+
   appOptions.store = store;
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
