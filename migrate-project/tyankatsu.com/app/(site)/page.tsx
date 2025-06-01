a03f2d299c1ec24be529ddd6718edec4adc1cc91
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2025-06-01 12:43:08 +1000

    wip

---
+++ migrate-project/tyankatsu.com/app/(site)/page.tsx
@@ -0,0 +1,11 @@
+// import { draftMode } from "next/headers";
+import styles from "./page.module.css";
+
+// import { getAllPosts } from "@/lib/api";
+
+export default async function Home() {
+  // const { isEnabled } = await draftMode();
+  // const allPosts = await getAllPosts(isEnabled);
+
+  return <main className={styles.main}>tyankatsu</main>;
+}
