a5d3f04bba456ea5490d357051bfa5de48353542
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2025-06-18 21:11:45 +1000

    feat: Open Graph画像のフォント読み込み機能を追加し、スタイルを調整

--- migrate-project/tyankatsu.com/app/(site)/posts/[slug]/opengraph-image.tsx
+++ migrate-project/tyankatsu.com/app/(site)/posts/[slug]/opengraph-image.tsx
@@ -9,6 +9,25 @@ export const size = {
 
 export const contentType = "image/png";
 
+async function loadGoogleFont(font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(
+    text
+  )}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/
+  );
+
+  if (resource) {
+    const response = await fetch(resource[1]);
+    if (response.status == 200) {
+      return await response.arrayBuffer();
+    }
+  }
+
+  throw new Error("failed to load font data");
+}
+
 export default async function Image({ params }: { params: { slug: string } }) {
   const post = await getPost(params.slug, false);
 
@@ -17,85 +36,51 @@ export default async function Image({ params }: { params: { slug: string } }) {
       <div
         style={{
           display: "flex",
-          gap: "20px",
           flexDirection: "column",
+          justifyContent: "space-between",
           background: "linear-gradient(135deg, #1a1e24 0%, #272c35 100%)",
           width: "100%",
           height: "100%",
-          fontSize: 88,
-          fontWeight: "bold",
-          color: "#00ff62",
-          lineHeight: 1.2,
-          textShadow:
-            "0 2px 10px rgba(0,255,98,0.4), 0 4px 20px rgba(0,0,0,0.4)",
+          padding: "60px",
         }}
       >
         <div
           style={{
-            padding: "60px",
+            fontSize: 70,
+            fontWeight: "bold",
+            color: "#00ff62",
+            lineHeight: 1.2,
+            textShadow:
+              "0 2px 10px rgba(0,255,98,0.4), 0 4px 20px rgba(0,0,0,0.4)",
           }}
         >
           {post.title}
         </div>
         <div
           style={{
-            borderTop: "1px solid rgba(255, 255, 255, 0.1)",
-            padding: "60px",
             fontSize: 30,
+            color: "#333",
+            backgroundColor: "#74f74b",
+            textShadow: "0 0 2px #333, 0 0 5px #333",
           }}
         >
           tyankatsu.com
         </div>
       </div>
-      // <div
-      //   style={{
-      //     background: "linear-gradient(135deg, #1a1e24 0%, #272c35 100%)",
-      //     width: "100%",
-      //     height: "100%",
-      //     padding: "60px",
-      //     position: "relative",
-      //   }}
-      // >
-      //   <div
-      //     style={{
-      //       height: "100%",
-      //       display: "flex",
-      //       alignItems: "center",
-      //       justifyContent: "center",
-      //     }}
-      //   >
-      //     <div
-      //       style={{
-      //         fontSize: 88,
-      //         fontWeight: "bold",
-      //         color: "#00ff62",
-      //         lineHeight: 1.2,
-      //         letterSpacing: "0.02em",
-      //         textShadow:
-      //           "0 2px 10px rgba(0,255,98,0.4), 0 4px 20px rgba(0,0,0,0.4)",
-      //         textAlign: "center",
-      //         maxWidth: "90%",
-      //       }}
-      //     >
-      //       {post.title}
-      //     </div>
-      //   </div>
-
-      //   <div
-      //     style={{
-      //       position: "absolute",
-      //       right: "60px",
-      //       bottom: "40px",
-      //       fontSize: 28,
-      //       color: "#ffffff",
-      //       letterSpacing: "0.05em",
-      //       textShadow: "0 2px 4px rgba(0,0,0,0.5)",
-      //       opacity: 0.8,
-      //     }}
-      //   >
-      //     tyankatsu.com
-      //   </div>
-      // </div>
-    )
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Geist",
+          data: await loadGoogleFont(
+            "DotGothic16",
+            `${post.title} tyankatsu.com`
+          ),
+          style: "normal",
+        },
+      ],
+    }
   );
 }
