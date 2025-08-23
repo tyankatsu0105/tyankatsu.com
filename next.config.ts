ffdc6bfc497fdc0c062b6b1a95c4532a453f4a57
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2025-08-23 23:08:21 +1000

    feat: distDirプロパティを削除し、next.config.tsを整理

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://images.ctfassets.net/pmz8kro2rz74/**")],
   },
-  distDir: "dist",
 };
 
 export default nextConfig;
